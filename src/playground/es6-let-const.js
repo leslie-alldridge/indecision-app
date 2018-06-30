14570ed0ac77ed1698ae488eec4ca6911746314d
Leslie Alldridge <lesliealldridge@Lisas-MacBook.local>
2018-06-30 22:03:02 +1200

    removed var from variables

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,18 @@
+var nameVar = 'Leslie';
+var nameVar = 'Leslie';
+console.log('nameVar', nameVar);
+
+let nameLet = 'Alan';
+console.log('nameLet', nameLet);
+
+const nameConst = 'Frank';
+console.log('nameConst', nameConst);
+
+var fullName = 'Leslie Alldridge';
+
+if(fullName){
+    var firstName = fullName.split(' ')[0];
+    console.log(firstName);
+}
+
+console.log(firstName);
