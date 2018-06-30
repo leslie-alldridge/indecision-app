6a7dc9f6be188e922299703ed6008227007959da
Leslie Alldridge <lesliealldridge@Lisas-MacBook.local>
2018-06-30 17:45:26 +1200

    added dependencies

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,10 @@
 console.log("app.js is running");
+
+var template = React.createElement(
+    "p",
+    null,
+    "this is jsx"
+  );
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
