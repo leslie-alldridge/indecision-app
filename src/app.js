b9ba51aeda12ed37aaa71731590f12e869cfcde0
Leslie Alldridge <lesliealldridge@Lisas-MacBook.local>
2018-07-06 06:50:08 +1200

    added new files and broke off code into component files

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
-//import './utlis.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-import subtract,{square, add} from './utlis.js';
-import isSenior,{isAdult, canDrink} from './person.js';
-console.log(subtract(10,10));
-// console.log(isAdult(19));
-// console.log(isAdult(10));
-// console.log(canDrink(19));
-// console.log(canDrink(22));
-console.log(isSenior(70));
-// console.log("app.js is running");
-// console.log(square(4));
-// console.log(add(2,50));
+
+  
+  
+  
+  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+  
